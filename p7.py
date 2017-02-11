4e799de303c77e7a7850721948165f9f0c0b836c
prashansa29l <prashansachaudhary8@gmail.com>
2017-02-11 23:12:27 +0530

    Create p7.py

---
+++ p7.py
@@ -0,0 +1,6 @@
+def thirdangle(angle1,angle2):
+    a3=180-(angle1+angle2)
+    print("thirdangle=",a3)
+a1=int(input("enter the first angle"))
+a2=int(input("enter the second angle"))
+thirdangle(a1,a2)
