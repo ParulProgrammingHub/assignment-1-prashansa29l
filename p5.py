e6be2cb528db2543fbddd0ca2aaefc5dcf1c38be
prashansa29l <prashansachaudhary8@gmail.com>
2017-02-11 23:10:17 +0530

    Create p5.py

---
+++ p5.py
@@ -0,0 +1,10 @@
+days=int(input("enter the days"))
+year=int(days/360)
+months=int(days/30)
+days=days%360
+months=months%30
+print (days)
+print (year)
+print (months)
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Stat
