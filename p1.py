5d258d56e75769817c19100d35c2921f4fbf7836
prashansa29l <prashansachaudhary8@gmail.com>
2017-02-11 22:59:17 +0530

    Create p1.py

---
+++ p1.py
@@ -0,0 +1,6 @@
+length=int(input("enter the length of rectangle:"))
+breadth=int(input("enter the breadth of rectangle:"))
+area=length*breadth
+perimeter=2*(length+breadth)
+print("/n area of rectangle is:%2d",%area)
+print("/n perimeter of rectangle is:%2d" %perimeter)
